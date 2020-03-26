ba12dfdb24bcec261f4338660798f670a7dab07d
Biubiupia <31363562+Biubiupia@users.noreply.github.com>
2020-03-26 20:18:37 +0800

    update on 3.26

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,12 +1,23 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.text.DateFormat;
+import java.util.Date;
+
+//@RestController
+@Controller
 public class HelloController {
-    @RequestMapping("/helloworld")
-    public String HelloWorld() {
-        return "Hello fuck!";
+    @Value("${name}")
+    private String name;
+
+    @RequestMapping("/hello")
+    public String HelloWorld(Model m) {
+        m.addAttribute("now", DateFormat.getDateTimeInstance().format(new Date()));
+        return "hello";
     }
 }
---
+++ src/main/java/com/example/demo/entity/Student.java
@@ -0,0 +1,12 @@
+package com.example.demo.entity;
+
+//@Entity
+//@Table(name = "student")
+public class Student {
+    private Integer id;
+    private String name;
+    private String position;
+    private String phonenum;
+    private String sex;
+    private String birth;
+}
