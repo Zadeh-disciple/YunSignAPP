159b11124ae259b88dc2c8696cd73852b30bedc8
Tong97 <33981860+Tong97@users.noreply.github.com>
2020-03-27 20:37:27 +0800

    Create 408.vue

---
+++ src/components/errorpages/408.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="ht-not-found not-found-default">
+          <div class="content">
+            <img src="../../assets/408.jpg" alt="404 image" width="560" height="428" />
+            <h2> 请求超时！！！</h2>
+          </div>
+          <div id="clouds">
+            <div class="cloud cloud-1"></div>
+            <div class="cloud cloud-2"></div>
+            <div class="cloud cloud-3"></div>
+            <div class="cloud cloud-4"></div>
+            <div class="cloud cloud-5"></div>
+            <div class="cloud cloud-6"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+</style>
