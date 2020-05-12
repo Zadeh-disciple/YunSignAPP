cc8b88a4b461bad76901c783a2133db3b72813f9
zjlin <1306326212@qq.com>
2020-05-12 14:56:38 +0800

    add pages

--- web/src/components/Home.vue
+++ web/src/components/Home.vue
@@ -1,19 +1,143 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+  <el-container style="height:100vh">
+    <el-header>
+      <div>
+        <img src="../assets/logo.png" alt />
+        <span>Yunsign</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <el-container>
+      <el-aside :width="isCollapse ? '64px' : '200px'">
+        <div class="toggle-button" @click="toggleCollapse">|||</div>
+        <el-menu
+          router
+          background-color="#333744"
+          text-color="#fff"
+          active-text-color="#409EFF"
+          unique-opened
+          :collapse="isCollapse"
+          :collapse-transition="false"
+          :default-active="$route.path"
+        >
+          <el-submenu :index="item.id+''" v-for="item in menulist" :key="item.id">
+            <template slot="title">
+              <i :class="iconObj[item.id]"></i>
+              <span>{{item.authName}}</span>
+            </template>
+            <el-menu-item :index="'/'+subItem.path" v-for="subItem in item.children" :key="subItem.id">
+              <template slot="title">
+                <i class="el-icon-menu"></i>
+                <span>{{subItem.authName}}</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      menulist: [],
+      iconObj: {
+        125: 'el-icon-user-solid',
+        103: 'el-icon-box',
+        102: 'el-icon-s-tools'
+      },
+      isCollapse: false
+    }
+  },
   methods: {
     logout () {
       window.sessionStorage.clear()
       this.$router.push('/login')
+    },
+    async getMenuList () {
+      const { data } = await this.$http.get('menus')
+      if (data.meta.status === 200) {
+        const list = []
+        for (let i = 0; i < data.data.length; i++) {
+          if (i <= 1) {
+            list[i] = data.data[i]
+            // console.log(data.data[i])
+          }
+          if (i === 3) {
+            data.data[i].authName = '系统管理'
+            data.data[i].children[0].authName = '数据字典'
+            data.data[i].children[0].path = 'dataDictionary'
+            list[i - 1] = data.data[i]
+          }
+        }
+        this.menulist = list
+        console.log(list)
+      } else {
+        this.$message({
+          type: 'error',
+          message: data.meta.msg
+        })
+      }
+    },
+    toggleCollapse () {
+      this.isCollapse = !this.isCollapse
     }
+  },
+  created () {
+    this.getMenuList()
   }
 }
 </script>
 
 <style lang="less" scoped>
+.el-header {
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+  > div {
+    display: flex;
+    align-items: center;
+    span {
+      margin-left: 15px;
+    }
+  }
+  img {
+    width: 50px;
+    height: 50px;
+  }
+}
+
+.el-aside {
+  background-color: #333744;
+  .el-menu {
+    border-right: none;
+  }
+}
+
+.el-main {
+  background-color: #eaedf1;
+}
+
+.iconfont {
+  margin-right: 10px;
+}
+
+.toggle-button {
+  background-color: #4a5064;
+  font-size: 10px;
+  line-height: 24px;
+  color: #fff;
+  text-align: center;
+  letter-spacing: 0.2em;
+  cursor: pointer;
+}
 </style>
--- web/src/router/index.js
+++ web/src/router/index.js
@@ -6,13 +6,29 @@ import Err404 from '../components/error-pages/404.vue'
 import Err403 from '../components/error-pages/403.vue'
 import Err500 from '../components/error-pages/500.vue'
 import Forgetpwd from '../components/forgetpwd.vue'
+import Welcome from '../components/welcome.vue'
+import DataDictionary from '../components/setting/data_dictionary.vue'
+import User from '../components/user/user.vue'
+import Right from '../components/power/Rights'
+import Role from '../components/power/Roles'
 
 Vue.use(VueRouter)
 
 const routes = [
   { path: '/', redirect: '/login' },
   { path: '/login', component: Login },
-  { path: '/home', component: Home },
+  {
+    path: '/home',
+    component: Home,
+    redirect: '/welcome',
+    children: [
+      { path: '/welcome', component: Welcome },
+      { path: '/users', component: User },
+      { path: '/dataDictionary', component: DataDictionary },
+      { path: '/rights', component: Right },
+      { path: '/roles', component: Role }
+    ]
+  },
   { path: '/404', component: Err404 },
   { path: '/401', component: Err403 },
   { path: '/500', component: Err500 },
